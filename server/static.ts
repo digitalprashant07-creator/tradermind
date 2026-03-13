76f749125006600f4025515a0296c0185317bd89
Prashant Maurya <digitalprashant07@Prashants-MacBook-Air.local>
2026-03-14 00:48:06 +0530

    fixed depoyment bugs

--- server/static.ts
+++ server/static.ts
@@ -1,10 +1,15 @@
 import express, { type Express } from "express";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function serveStatic(app: Express) {
-  const clientDistPath = path.resolve(import.meta.dirname, "..", "dist", "client");
+  const clientDistPath = path.resolve(__dirname, "..", "dist", "public");
+
   app.use(express.static(clientDistPath));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(clientDistPath, "index.html"));
+
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(clientDistPath, "index.html"));
   });
 }
